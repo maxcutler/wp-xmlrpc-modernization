bd9b5e13f2cf10f9814d24a890fa8776436797e5
Max Cutler <max@maxcutler.com>
2012-12-02 19:18:28 +0000

    Fix PHPDoc typo.

--- class-wp-xmlrpc-server-ext.php
+++ class-wp-xmlrpc-server-ext.php
@@ -876,7 +876,7 @@ class wp_xmlrpc_server_ext extends wp_xmlrpc_server {
 	 *      - post_status (default: 'draft')
 	 *      - post_title
 	 *      - post_author
-	 *      - post_exerpt
+	 *      - post_excerpt
 	 *      - post_content
 	 *      - post_date_gmt | post_date
 	 *      - post_format
